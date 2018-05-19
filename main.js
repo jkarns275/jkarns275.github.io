4999ab3e64d30621c746ac23eb2d713e2302d6b3
Joshua Karns <jkarns275@gmail.com>
2018-05-18 22:10:43 -0400

    Changed port for aws

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ var proxy = require('http-proxy-middleware');
 var app = express();
 
 app.use('/', proxy({target: 'http://cs.oswego.edu/~jkarns', changeOrigin: true}));
-app.listen(8080);
+app.listen(80);
